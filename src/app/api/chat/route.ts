7976da1568ab36d11509fab816cc04bc372976a8
harrischen <harrischen@tencent.com>
2023-04-21 23:28:47 +0800

    fix: remove unused file

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -26,7 +26,7 @@ export async function POST(req: Request): Promise<Response> {
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
-    max_tokens: 1000,
+    max_tokens: 50,
     stream: true,
     n: 1,
   };
