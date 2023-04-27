431850499068a47661ea6539159a0cc6b239c174
harrischen <harrischen@tencent.com>
2023-04-27 19:26:31 +0800

    feat: 添加一个to方法，解决try...catch的优雅写法

--- src/common/helper.ts
+++ src/common/helper.ts
@@ -14,9 +14,9 @@ const markdownItMap = {
 
 /**
  * 格式化消息
- * @param message 
- * @param mode 
- * @returns 
+ * @param message
+ * @param mode
+ * @returns
  */
 export function formatMessage(message?: string, mode = FormatMessageMap.zero) {
   let result = message?.trim();
@@ -32,4 +32,18 @@ export function formatMessage(message?: string, mode = FormatMessageMap.zero) {
   result = result.replace(/==SPACE=PLACEHOLDER==/g, "&nbsp;");
   result = result.replace(/==BREAK=PLACEHOLDER==/g, "<br>");
   return result;
+}
+
+/**
+ * 错误处理方法
+ * 避免主要逻辑里面一大段的try catch
+ * const [data, err] = await to(promise)
+ * @param promise
+ */
+export async function to<T>(promise: Promise<T>): Promise<[T, any]> {
+  try {
+    return [await promise, null] as [T, any];
+  } catch (e) {
+    return [null, e] as [any, any];
+  }
 }
