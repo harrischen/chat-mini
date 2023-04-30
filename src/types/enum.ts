004d1b5d2bb836badd747e03f26838c8901fa9ae
harrischen <harrischen@tencent.com>
2023-04-30 08:58:59 +0800

    fix: 整理enum，去除不再需要的声明

--- src/types/enum.ts
+++ src/types/enum.ts
@@ -1,28 +1,9 @@
 export enum CompletionModelMap {
   "gpt-3.5-turbo" = "gpt-3.5-turbo",
   "gpt-3.5-turbo-0301" = "gpt-3.5-turbo-0301",
-  "gpt-4" = "gpt-4",
-  "gpt-4-0314" = "gpt-4-0314",
-  "gpt-4-32k" = "gpt-4-32k",
-  "gpt-4-32k-0314" = "gpt-4-32k-0314",
 }
 
-export enum HttpMethod {
-  GET = "GET",
-  POST = "POST",
-}
-
-export enum HttpStatusCode {
-  OK = 200,
-  ParamsError = 400,
-}
-
-/**
- * 角色
- * 参考 https://github.com/openai/openai-node/blob/master/api.ts
- */
 export enum Role {
-  system = "system",
   user = "user",
   assistant = "assistant",
 }
